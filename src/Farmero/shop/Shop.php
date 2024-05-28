2fa5863a71d5e8146a225c66fdf07d31497be019
Farmero14 <145240944+Farmero14@users.noreply.github.com>
2024-05-28 12:20:45 -0700

    Update Shop.php

--- src/Farmero/shop/Shop.php
+++ src/Farmero/shop/Shop.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Farmero\shop;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
+use pocketmine\Server;
 
 use Farmero\shop\Command\ShopCommand;
 use Farmero\shop\Forms\ShopForm;
@@ -18,6 +18,10 @@ class Shop extends PluginBase {
         $this->saveResource("shop.yml");
         $this->shopForm = new ShopForm($this);
         $this->getServer()->getCommandMap()->register("shop", new ShopCommand($this));
+        if (Server::getInstance()->getPluginManager()->getPlugin("MoneySystem") === null) {
+            $this->getLogger()->info("Disabling Shop, MoneySystem not found... Please make sure to have it installed before trying again!");
+            Server::getInstance()->getPluginManager()->disablePlugin($this);
+            return;
     }
 
     public function getShopForm(): ShopForm {
